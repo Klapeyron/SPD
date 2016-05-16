11c0d3f720927834b8b5c3289e173a226bd264d9
Klapeyron <t.m@o2.pl>
2016-05-16 22:22:35 +0200

    DEbug prints

--- algorithms/CalierAlgorithm.cpp
+++ algorithms/CalierAlgorithm.cpp
@@ -4,7 +4,9 @@ int UB = std::numeric_limits<int>::max();
 
 int calier(std::vector<Task> tasks)
 {
+  std::cout << "CARLIER" << std::endl;
   int U = schrage(tasks);
+  std::cout << "U i UB :" << U << " " << UB << std::endl;
   if(U < UB)
     UB = U;
 
@@ -12,24 +14,29 @@ int calier(std::vector<Task> tasks)
   int a = CalierBlocks::calculateA(tasks, b, UB);
   int c = CalierBlocks::calculateC(tasks, a, b);
 
-  std::cout << a << " " << b << " " << c << " " << std::endl << std::endl;
+  std::cout << "abc " << a << " " << b << " " << c << " " << std::endl << std::endl;
 
   if(c == -1)
+  {
+    std::cout << "Returning UB " << UB << std::endl;
     return UB;
+  }
 
   auto orderByR = [](Task const& left, Task const& right) { return left.r < right.r; };
   auto orderByQ = [](Task const& left, Task const& right) { return left.q < right.q; };
 
-  auto rP = std::min_element(tasks.begin()+c+1, tasks.begin()+b, orderByR);
-  auto qP = std::min_element(tasks.begin()+c+1, tasks.begin()+b, orderByQ);
-  auto pP = std::accumulate(tasks.begin()+c+1, tasks.begin()+b, 0,
+  auto rP = std::min_element(tasks.begin()+c+1, tasks.begin()+b+1, orderByR);
+  auto qP = std::min_element(tasks.begin()+c+1, tasks.begin()+b+1, orderByQ);
+  auto pP = std::accumulate(tasks.begin()+c+1, tasks.begin()+b+1, 0,
                             [](int sum, Task const& task)  {
                               return sum + task.p;
                             });
-
+  std::cout << (*rP).r << " " << (*qP).q << " " << pP << " primy" << std::endl;
+  
   auto taskC_r_Copy = tasks[c];
   tasks[c].r = std::max(tasks[c].r, (*rP).r + pP);
   auto LB = schrageWithDivision(tasks);
+  std::cout << "LB z UB: " << LB << " " << UB << std::endl;
   if(LB < UB)
     calier(tasks);
 
@@ -39,6 +46,7 @@ int calier(std::vector<Task> tasks)
   auto taskC_q_Copy = tasks[c];
   tasks[c].q = std::max(tasks[c].q, (*qP).q + pP);
   LB = schrageWithDivision(tasks);
+  std::cout << "2 czesc U i UB :" << U << " " << UB << std::endl;
   if(LB < UB)
     calier(tasks);
 
